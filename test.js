601f42d0dd371d1b8b3bdc1a3c61c940ad32e3ab
Mike Basdeo <mikebasdeo@gmail.com>
2020-06-10 08:51:58 -0400

    Update test.js

--- test.js
+++ test.js
@@ -23,9 +23,24 @@ const student = new mongoose.Schema({
       type: Number,
     },
   },
+  school: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'school',
+  },
+})
+
+// School Schema
+const school = new mongoose.Schema({
+  name: String,
+  openSince: Number,
+  students: Number,
+  isGreat: Boolean,
+  staff: [{ type: String }],
 })
 
 // create a model
+const School = mongoose.model('school', school)
 const Student = mongoose.model('student', student)
 
 //inserts and queries
@@ -34,9 +49,54 @@ const Student = mongoose.model('student', student)
 // retrieve it
 connect()
   .then(async (connection) => {
-    const student = await Student.create({ firstName: 'Mike' })
-    const find = await student.find({ firstName: 'Randy' })
-    const findById = await student.findById({ firstName: '5c0923cnnbcn' })
-    const updated = await student.findByIdAndUpdate('asda', {})
+    const school1 = {
+      name: 'String',
+      openSince: 3,
+      students: 3,
+      isGreat: true,
+      staff: ['a', 'b', 'c'],
+    }
+
+    const school2 = {
+      name: 'String2',
+      openSince: 6,
+      students: 6,
+      isGreat: false,
+      staff: ['v', 'b', 'g'],
+    }
+
+    const schools = await School.create([school1, school2])
+
+    // const match = await School.findOne({
+    //   students: { $gt: 4 },
+    //   isGreat: false,
+    // })
+
+    // const match = await School.find({
+    //   staff: 'b',
+    // })
+
+    const match = await School.find({
+      $in: { staff: ['v', 'b', 'g'] },
+    })
+      .sort({ openSince: 1 })
+      .limit(2)
+      .exec()
+
+    console.log(match)
+    // const school = await School.create({ name: 'Wilkinson' })
+
+    // const student = await Student.create({
+    //   firstName: 'Steve',
+    //   school: school._id,
+    // })
+
+    // const match = await Student.findById(student.id).populate('school').exec()
+    // Archive:
+    // const mike = await Student.findOne({ firstName: 'Mike' }).exec()
+    // const student = await Student.create({ firstName: 'Mike' })
+    // const find = await student.find({ firstName: 'Randy' })
+    // const findById = await student.findById({ firstName: '5c0923cnnbcn' })
+    // const updated = await student.findByIdAndUpdate('asda', {})
   })
   .catch((e) => console.error(e))
